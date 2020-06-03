c0ce5a0a3af10d5294f3b7c9c5ab36eda2061567
corina <corina.schambacher@gmail.com>
2020-06-03 14:39:08 -0700

    Updates

---
+++ precache-manifest.24b06017fc4c859319bed21930e91e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3792a61562ed7f45c1d1bedb28ac823d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a4b9339e6449c2fdaf73",
+    "url": "/monsters-rolodex/static/css/main.8c29ac25.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b9339e6449c2fdaf73",
+    "url": "/monsters-rolodex/static/js/main.d6413620.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
